bebcbb9ffd0d68cef0b6297b55b67ef93f41c429
BesarAjruli <aquila1912@gmail.com>
2025-10-18 16:12:06 +0200

    Fixed some UI and UX bugs, added year removed price and buy button

--- client/src/Components/Books.jsx
+++ client/src/Components/Books.jsx
@@ -1,33 +1,5 @@
 import "./Books.css";
-
-const books = [
-  {
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    price: "$10.35",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg",
-  },
-  {
-    title: "48 laws of power",
-    author: "Robert Greene",
-    price: "$75.99",
-    image: "https://m.media-amazon.com/images/I/611X8GI7hpL.jpg",
-  },
-  {
-    title: "Rich dad poor dad",
-    author: "Robert T. Kiyosaki",
-    price: "$15.55",
-    image: "https://bookcorner.mk/storage/images/products/18/front.jpg",
-  },
-  {
-    title: "Harry Potter and...",
-    author: "J.K. Rowling",
-    price: "$69.45",
-    image:
-      "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale,dpr_1.5/jackets/9781408855669.jpg",
-  },
-];
+import books from "./booksJson";
 
 const Books = () => {
   return (
@@ -41,8 +13,7 @@ const Books = () => {
             <span>{book.title}</span>
             <span className="suggAuth">{book.author}</span>
             <div>
-              <span className="suggPrice">{book.price}</span>
-              <span className="suggBuy">Buy</span>
+              <span className="suggYear">{book.year}</span>
             </div>
           </div>
         </div>
