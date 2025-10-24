6ce475da41091b6970b518def26bb6d98882aac8
BesarAjruli <aquila1912@gmail.com>
2025-10-24 15:04:21 +0200

    Added footer files

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2025 Besar Ajruli. All rights reserved.</p>
+        </footer>
+    )
+};
+
+export default Footer;
